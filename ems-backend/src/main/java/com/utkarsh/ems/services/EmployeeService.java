7e2ede7eb55352bd57ae70fbe655106186d0368d
utkarsh <utkarshmalik19@gmail.com>
2025-04-26 00:37:19 +0530

    Added Get All Employees Logic

--- ems-backend/src/main/java/com/utkarsh/ems/services/EmployeeService.java
+++ ems-backend/src/main/java/com/utkarsh/ems/services/EmployeeService.java
@@ -2,8 +2,11 @@ package com.utkarsh.ems.services;
 
 import com.utkarsh.ems.dto.EmployeeDto;
 
+import java.util.List;
+
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
     EmployeeDto getEmployeeById(Long employeeId);
+    List<EmployeeDto> getAllEmployees();
 
 }
--- ems-backend/src/main/java/com/utkarsh/ems/servicesimpl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/com/utkarsh/ems/servicesimpl/EmployeeServiceImpl.java
@@ -9,6 +9,9 @@ import com.utkarsh.ems.services.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
@@ -17,6 +20,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = employeeRepository.findById(employeeId)
                 .orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with given id"));
         return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
     }
 
     private EmployeeRepository employeeRepository;
