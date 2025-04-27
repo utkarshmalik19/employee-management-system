fbed5acf7c9d9f5091685fd49fc8879da5a7d26c
utkarsh <utkarshmalik19@gmail.com>
2025-04-27 22:46:17 +0530

    Added List Employee Component

---
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+
+const ListEmployeeComponent = () => {
+    const dummyData = [
+        {
+            "id" : "1",
+            "firstName" : "Ram",
+            "lastName" : "def",
+            "email" : "ram@gmail.com"
+        },
+        {
+            "id" : "2",
+            "firstName" : "Raj",
+            "lastName" : "def",
+            "email" : "raj@gmail.com"
+        }
+    ]
+    return (
+    <div>
+        <h2>List of Employees</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Employee ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email ID</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    dummyData.map(employee => 
+                        <tr key={employee.id}>
+                            <td>{employee.id}</td>
+                            <td>{employee.firstName}</td>
+                            <td>{employee.lastName}</td>
+                            <td>{employee.email}</td>
+                        </tr>
+
+                    )
+                }
+            </tbody>
+        </table>
+    </div>  
+)
+}
+
+export default ListEmployeeComponent
