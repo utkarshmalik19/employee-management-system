8ba87b3973af3ae487fbd77cb163b119007316fb
utkarsh <utkarshmalik19@gmail.com>
2025-04-28 21:23:00 +0530

    Added Footer Component

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 
@@ -9,6 +10,7 @@ function App() {
     <>
     <HeaderComponent />
       <ListEmployeeComponent/>
+      <FooterComponent />
     </>
   )
 }
