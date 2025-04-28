f8d6affaee4aa4c92d73335f5d3b274b61d72413
utkarsh <utkarshmalik19@gmail.com>
2025-04-28 22:58:52 +0530

    Added Routes Logic

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -2,15 +2,21 @@ import './App.css'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
 
 function App() {
   
 
   return (
     <>
+    <BrowserRouter>
     <HeaderComponent />
-      <ListEmployeeComponent/>
+    <Routes>
+      <Route path='/' element ={<ListEmployeeComponent/>}></Route>
+      <Route path='/employees' element = {<ListEmployeeComponent/>}></Route>
+    </Routes>
       <FooterComponent />
+      </BrowserRouter>
     </>
   )
 }
