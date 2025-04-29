6e039105cf6c16627627e1e428ecbb9c4b4a9bb4
utkarsh <utkarshmalik19@gmail.com>
2025-04-29 22:27:38 +0530

    Added Form Validation to Add Employee Component

--- ems-frontend/src/components/EmployeeComponent.jsx
+++ ems-frontend/src/components/EmployeeComponent.jsx
@@ -6,18 +6,50 @@ const EmployeeComponent = () => {
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
+    const [errors,setErrors] =useState({
+        firstName: '',
+        lastName: '',
+        email: ''
+    })
     const navigator = useNavigate();
 
     function saveEmployee(e){
         e.preventDefault();
-        const employee = {firstName,lastName,email}
+        if(validateForm()){
+            const employee = {firstName,lastName,email}
         console.log(employee)
         createEmployee(employee).then((response) => {
             console.log(response.data);
             navigator('/employees')
         })
+        }
+        
     }
 
+    function validateForm(){
+        let valid = true;
+        const errorsCopy = {... errors}
+        if(firstName.trim()){
+            errorsCopy.firstName = '';
+        }else{
+            errorsCopy.firstName = 'First Name is required';
+            valid = false;
+        }
+        if(lastName.trim()){
+            errorsCopy.lastName = '';
+        }else{
+            errorsCopy.lastName = 'Last Name is required';
+            valid = false;
+        }
+        if(email.trim()){
+            errorsCopy.email = '';
+        }else{
+            errorsCopy.email = 'Email is required';
+            valid = false;
+        }
+        setErrors(errorsCopy);
+        return valid;
+    }
     return (
     <div className='container'>
         <br></br>
@@ -33,11 +65,12 @@ const EmployeeComponent = () => {
                             placeholder='Enter Employee First Name'
                             name='firstname'
                             value={firstName}
-                            className='form-control'
+                            className={`form-control ${errors.firstName ? 'is-invalid': ''}`}
                             onChange={(e) => setFirstName(e.target.value)}
                             >
 
                             </input>
+                            {errors.firstName && <div className='invalid-feedback'>{errors.firstName}</div>}
                         </div>
                         <div className='form-group mb-2'>
                             <label className='form-label'>Last Name</label>
@@ -46,11 +79,12 @@ const EmployeeComponent = () => {
                             placeholder='Enter Employee Last Name'
                             name='lastname'
                             value={lastName}
-                            className='form-control'
+                            className={`form-control ${errors.lastName  ? 'is-invalid': ''}`}
                             onChange={(e) => setLastName(e.target.value)}
                             >
 
                             </input>
+                            {errors.lastName && <div className='invalid-feedback'>{errors.lastName}</div>}
                         </div>
                         <div className='form-group mb-2'>
                             <label className='form-label'>Email</label>
@@ -59,11 +93,12 @@ const EmployeeComponent = () => {
                             placeholder='Enter Employee Email'
                             name='email'
                             value={email}
-                            className='form-control'
+                            className={`form-control ${errors.email  ? 'is-invalid': ''}`}
                             onChange={(e) => setEmail(e.target.value)}
                             >
 
                             </input>
+                            {errors.email && <div className='invalid-feedback'>{errors.email}</div>}
                         </div>
                         <button className='btn btn-success' onClick={saveEmployee}>Submit</button>
                     </form>
